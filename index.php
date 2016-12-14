90b187542af6a676b83923802d0487ffb80fb99a
Saidur <saidur@rudias.com>
2016-12-14 16:50:13 +1100

    try

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 
 header('Access-Control-Allow-Origin: *');
 // Tell PHP that we're using UTF-8 strings until the end of the script
-mb_internal_encoding('UTF-8');
-
-// Tell PHP that we'll be outputting UTF-8 to the browser
-mb_http_output('UTF-8');
+//mb_internal_encoding('UTF-8');
+//
+//// Tell PHP that we'll be outputting UTF-8 to the browser
+//mb_http_output('UTF-8');
 
 require('vendor/autoload.php');
 
