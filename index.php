4dee30293a325437081a50b04ca0431443197cfe
Saidur <saidur@rudias.com>
2016-12-14 16:48:21 +1100

    ddeploy

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 
-//header('Access-Control-Allow-Origin: *');
-//// Tell PHP that we're using UTF-8 strings until the end of the script
-//mb_internal_encoding('UTF-8');
-//
-//// Tell PHP that we'll be outputting UTF-8 to the browser
-//mb_http_output('UTF-8');
+header('Access-Control-Allow-Origin: *');
+// Tell PHP that we're using UTF-8 strings until the end of the script
+mb_internal_encoding('UTF-8');
+
+// Tell PHP that we'll be outputting UTF-8 to the browser
+mb_http_output('UTF-8');
 
 require('vendor/autoload.php');
 
@@ -24,14 +24,14 @@ Router::route('geo/gettopartist/:country/:page',
         return (new GetTopArtistsByGeography($config))->makeRequestBy($country, $page);
     }
 );
-//
-//
-//Router::route('artist/gettoptrack/:artist/:page',
-//    function($segment, $method, $artist, $page = 1) use ($config)
-//    {
-//        return (new GetTopTracksByArtist($config))->makeRequestBy($artist, $page);
-//    }
-//);
-//
+
+
+Router::route('artist/gettoptrack/:artist/:page',
+    function($segment, $method, $artist, $page = 1) use ($config)
+    {
+        return (new GetTopTracksByArtist($config))->makeRequestBy($artist, $page);
+    }
+);
+
 Router::execute($_SERVER['REQUEST_URI']);
 
